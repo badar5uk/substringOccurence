a9f248e61e41b699c8de1cf734312efb05ea7276
bader <bader5uk@hotmail.com>
2024-09-23 10:08:49 +0400

    Removed unecessary code

--- substringOccurence.java
+++ substringOccurence.java
@@ -1,22 +1,13 @@
 public class substringOccurence {
-    public static String countOccurrences() {
-       String str = "I love coding, I love coding in Java";
-       String substr = str.substring(0);
-        return substr;
-    }
 
-    public static void counter(){
+    public static void main(String[] args) {
+        String str = "I love coding, I love coding in Java";
+        String substr = str.substring(0);
         int counter = 0;
-        if (countOccurrences().contains("coding")){
+        if (substr.contains("coding")) {
             counter++;
             System.out.println("counter is:" + counter);
         }
-    }
-
-    public static void main(String[] args) {
-        System.out.println(countOccurrences());
-        counter();
-
     }
 }
 
